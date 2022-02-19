a6e62cf73e721a0fc5dcde716d0046b4a9da59df
gtiszjj <896234291@qq.com>
2022-02-19 13:38:26 +0800

    增加最近两次领跑的间隔

--- src/main/java/com/gis/gp/entity/BkDayInfo.java
+++ src/main/java/com/gis/gp/entity/BkDayInfo.java
@@ -3,6 +3,7 @@ package com.gis.gp.entity;
 /**
  * Created by GIS on 2022/2/12.
  * 距离上次领跑相隔天数信息
+ * a.bkmc,a.maxtjrq as pretjrq,b.maxtjrq, b.maxtjrq - a.maxtjrq as pretwodelta,b.deltadays
  */
 public class BkDayInfo {
     public String getBkmc() {
@@ -30,6 +31,35 @@ public class BkDayInfo {
     }
 
     private String bkmc;
+    private String pretjrq;
+
+    public String getPretjrq() {
+        return pretjrq;
+    }
+
+    public void setPretjrq(String pretjrq) {
+        this.pretjrq = pretjrq;
+    }
+
+    public String getPretwodelta() {
+        return pretwodelta;
+    }
+
+    public void setPretwodelta(String pretwodelta) {
+        this.pretwodelta = pretwodelta;
+    }
+
+    private String pretwodelta;
     private String maxtjrq;
     private String deltadays;
+
+    public String getMybkmc() {
+        return mybkmc;
+    }
+
+    public void setMybkmc(String mybkmc) {
+        this.mybkmc = mybkmc;
+    }
+
+    private String mybkmc;
 }
