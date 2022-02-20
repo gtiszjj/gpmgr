7213ebb3a08b2b632b8c7a6bfb3d56ac3ddb531d
gtiszjj <896234291@qq.com>
2022-02-20 11:39:22 +0800

    增加跨域访问注解

--- src/main/java/com/gis/gp/controller/BkController.java
+++ src/main/java/com/gis/gp/controller/BkController.java
@@ -8,6 +8,7 @@ import com.gis.gp.service.BkInfoService;
 import com.gis.gp.service.impl.BkInfoServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -42,6 +43,7 @@ public class BkController {
         return sResult;
     }
 
+    @CrossOrigin
     @RequestMapping("/getBkDayInfo")
     @ResponseBody
     public String getBkDayInfo() throws JsonProcessingException {
